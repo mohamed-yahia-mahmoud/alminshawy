abc81ca575fc14e06d7740c2ddf127dfc1a8783a
macbookpro <mohamed.yahia107@yahoo.com>
2021-09-21 18:31:51 +0200

    adding new ads

--- lib/statefull/Ad_helper.dart
+++ lib/statefull/Ad_helper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelper{
   static String get bannerAdUnitId{
     if(Platform.isAndroid) {
-      return "ca-app-pub-8165719959159108/3995703022";
+      return "ca-app-pub-8165719959159108/2198610433";
     }else if(Platform.isIOS){
       return "ca-app-pub-8165719959159108/7382504997";
     }else{
